385d1a5dd9bf005253b71f5a442fee44f1081733
zerosauce <underworld.paradox@gmail.com>
2014-09-02 23:08:55 -0500

    Updated paper controllers to interact with db

--- controllers/papers.js
+++ controllers/papers.js
@@ -1,34 +1,100 @@
 var mongoose = require('mongoose')
 var papers = require('../models/papers');
 
-
-module.exports.controller = function(app) {
+module.exports.controller = function(app, express) {
 
     //validate all requests
-    app.all('/papers*', function(req, res, next){
+    app.all('/papers/*', function(req, res, next){
         console.log('validating integrity of paper...');
         next();
     });
 
+    //load papers view
+    app.get('/papers', function(req, res){
+        console.log('views not yet implemented');
+        res.render('papers');
+    })
+
     //get all papers
-    app.get('/papers/:id', function(req, res) {
+    app.get('/papers/all', function(req, res) {
+        Papers.find(function(err, papers){
+            if(err)
+                res.send(err);
+
+            res.json(papers);
+        });
+    });
+
+    //get specific papers
+    app.get('/papers/id/:id', function(req, res) {
         var id = req.params['id'];
-        if(id){
-            res.send('You requested papers #' + id);
-        }
-        else{
-            res.send('You requested all papers');
-        }
+        Papers.find({id: id}, function(err, papers){
+            if(err)
+                res.send(err);
+
+            res.json(papers);
+        });
+    });
+
+    app.get('/papers/name/:name', function(req, res) {
+        var name = req.params['name'];
+        Papers.find({name: name}, function(err, papers){
+            if(err)
+                res.send(err);
+
+            res.json(papers);
+        });
+    });
+
+    app.get('/papers/url/:url', function(req, res) {
+        var url = req.params['url'];
+        Papers.find({url : url}, function(err, papers){
+            if(err)
+                res.send(err);
+
+            res.json(papers);
+        });
     });
 
     //update a paper
     app.put('/papers/:id', function(req, res) {
         var id = req.params['id'];
-        res.send('You requested to update paper #' + id);
+        var name = req.params['name'];
+        var url = req.params['url'];
+        Papers.update({id: id}, {name: name, url: url}, function(err, numAffected, raw){
+            if(err)
+                res.send(err);
+            res.send('Updated %d document(s))', numAffected);
+        })
     });
 
     //create a new paper
     app.post('/papers', function(req, res) {
-        res.send('Added new paper entry');
+        var name = req.body['name'];
+        var url = req.body['url'];
+        console.log(req.body);
+        if(!name || !url){
+            res.send(400, 'Document invalid');
+            return;
+        }
+        Papers.create({
+            name: name,
+            url: url
+        }, function(err, paper){
+            if(err)
+                res.send('err');
+            res.send('successfully saved paper: ' + paper);
+        });
+    });
+
+    //delete a paper
+    app.delete('/papers/:id', function(req, res){
+        console.log('user authentication not implemented')
+        var id = req.params['id'];
+        Papers.findByIdAndRemove(id, function(err){
+            if(err)
+                res.send(err);
+            res.send('Successfully deleted document');
+        });
     });
 }
